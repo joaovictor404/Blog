6458e2de1e26f64c28e3e18d270b0660eb2ff0dc
joaovictor404 <joaovictorlira05@gmail.com>
2024-08-08 09:33:26 -0300

    third commit

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Modalidade, Aluno, Curso
 # Register your models here.
+
+@admin.register(Modalidade)
+class ModalidadeAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+    list_filter = ('nome',)
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ('nome','idade','cpf','email','data_nascimento',)
+    search_fields = ('nome','idade','cpf','email','data_nascimento',)
+    list_filter = ('nome','idade','cpf','email','data_nascimento',)
+
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nome','descricao','modalidade',)
+    search_fields = ('nome','descricao',)
+    list_filter = ('nome','descricao',)
--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Modalidade(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    cpf = models.CharField(max_length=11)
+    email = models.EmailField()
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+class Curso(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    modalidade = models.ForeignKey(Modalidade, on_delete=models.CASCADE)
+    aluno = models.ManyToManyField(Aluno, blank=True,null=True)
+
+    def __str__(self):
+        return self.nome
+
+
+
