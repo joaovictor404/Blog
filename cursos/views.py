614f21fc0426cab6a5e0b81e20262533226d6f93
joaovictor404 <joaovictorlira05@gmail.com>
2024-08-13 15:26:40 -0300

    quarter commit

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Curso, Modalidade, Aluno 
 # Create your views here.
+
+def index(request):
+    alunos = Aluno.objects.all()
+    contexto = {
+        'alunos': alunos,
+    }
+    return render(request, 'cursos/index.html', contexto)
